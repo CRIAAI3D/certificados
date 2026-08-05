a332c6b80e25230b162623493ddecad322da8462
Fígaz <vmfp942@gmail.com>
2026-08-05 17:59:01 -0400

    Adiciona assets do sistema

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,16 @@
+// Cole aqui as configurações do Firebase do seu novo projeto.
+// Firebase Console > Configurações do projeto > Seus apps > Web app.
+window.firebaseConfig = {
+  apiKey: "COLE_AQUI",
+  authDomain: "COLE_AQUI.firebaseapp.com",
+  projectId: "COLE_AQUI",
+  storageBucket: "COLE_AQUI.firebasestorage.app",
+  messagingSenderId: "COLE_AQUI",
+  appId: "COLE_AQUI"
+};
+
+// Nome público usado no validador e nos registros.
+window.extremoConfig = {
+  sistemaNome: "Certificados Extremo",
+  unidadePadrao: "Extremo Norte"
+};
