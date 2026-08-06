c66ce3d22d1c81383b9a7a91e8a1ae1d9f053602
Fígaz <vmfp942@gmail.com>
2026-08-06 08:16:57 -0400

    Update Firebase configuration and add extremoConfig

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,9 +1,15 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyA7668jV51rQrkPdxNlpe5BCMPnmRZQrTk",
+// Configurações do Firebase do projeto cert-generator.
+window.firebaseConfig = {
+  apiKey: "AIzaSyA766jV5IrQkrPdxNlpe5BCMPnmRZQrtk",
   authDomain: "cert-generator-3b9bb.firebaseapp.com",
   projectId: "cert-generator-3b9bb",
   storageBucket: "cert-generator-3b9bb.firebasestorage.app",
   messagingSenderId: "1000455716885",
   appId: "1:1000455716885:web:14f4df8d66f5f9c8cb4e1c",
-  measurementId: "G-4FWFN48XX5"
+  measurementId: "G-4FWFNA8XX5"
+};
+
+window.extremoConfig = {
+  sistemaNome: "Certificados Extremo",
+  unidadePadrao: "Extremo Norte"
 };
