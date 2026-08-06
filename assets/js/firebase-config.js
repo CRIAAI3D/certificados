6d7df09868d328e32d152c7cc688825bdb86da33
Fígaz <vmfp942@gmail.com>
2026-08-06 07:31:14 -0400

    Refactor firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,14 +1,9 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSyA766jV5IrQkrPdxNlpe5BCMPnmRZQztk",
+const firebaseConfig = {
+  apiKey: "AIzaSyA7668jV51rQrkPdxNlpe5BCMPnmRZQrTk",
   authDomain: "cert-generator-3b9bb.firebaseapp.com",
   projectId: "cert-generator-3b9bb",
   storageBucket: "cert-generator-3b9bb.firebasestorage.app",
   messagingSenderId: "1000455716885",
   appId: "1:1000455716885:web:14f4df8d66f5f9c8cb4e1c",
-  measurementId: "G-4FWFNA8XX5"
-};
-
-window.extremoConfig = {
-  sistemaNome: "Certificados Extremo",
-  unidadePadrao: "Extremo Norte"
+  measurementId: "G-4FWFN48XX5"
 };
